bc6c3dcfcfe68ec3f91f6cb371abd49cd77905a1
wschc <2890841553@qq.com>
2015-11-01 23:40:58 +0630

    disicizuoye

--- works/1314080901202/js/javascript.js
+++ works/1314080901202/js/javascript.js
@@ -1,1 +1,25 @@
++// create a new module called 'myAppModule' and save 
++// a reference to it in a variable called myAppModule 
++var orderModule = angular.module('orderModule', []);
++
++// use the myAppModule variable to
++// configure the module with a controller
++orderModule.controller('createOrderCtrl', function($scope) {
++	var banjianlist = {
+            ceban: '侧板',
+            cengban: '层板',
+            menban: '门板',
+            beiban: '背板',            
+            mupi: '木皮',
++	};
++	$scope.data = banjianlist;
++});
++
++// use the myAppModule variable to
++// configure the module with a filter
++myAppModule.filter('stripDashes', function() {
++	return function(txt) {
++		// filter code would go here
++	};
++}); 
 // javascript.js
