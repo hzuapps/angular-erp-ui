73255223c9c9c15122cead52caee2c4ae4d2dfa3
wszjx <1289245883@qq.com>
2015-11-01 22:42:40 +0630

    第四次作业提交

--- works/1314080901242/js/javascript.js
+++ works/1314080901242/js/javascript.js
@@ -1,1 +1,27 @@
++// create a new module called 'myAppModule' and save 
++// a reference to it in a variable called myAppModule 
++var orderModule = angular.module('orderModule', []);
++
++// use the myAppModule variable to
++// configure the module with a controller
++orderModule.controller('createOrderCtrl', function($scope) {
++	var BOMmessage = {
++		  dingdan: '某某公司装修工程',
+          chanping: '楼层装饰',
+          data1: '2015.9.1',
+          data2: '2015.9.31',
+          status: '装修中',
+          caidanyuan: '小张',
+          beizhu: '无',
++	};
++	$scope.data = BOMmessage;
++});
++
++// use the myAppModule variable to
++// configure the module with a filter
++myAppModule.filter('stripDashes', function() {
++	return function(txt) {
++		// filter code would go here
++	};
++}); 
 // javascript.js
