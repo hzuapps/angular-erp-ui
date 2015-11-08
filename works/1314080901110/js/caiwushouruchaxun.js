df2adc5344869b922b503b649d5a27e69b02e2b7
hebinhai <15767956765@163.com>
2015-11-08 22:41:50 +0800

    实验4修改-1314080901110
    实验4修改

--- works/1314080901110/js/caiwushouruchaxun.js
+++ works/1314080901110/js/caiwushouruchaxun.js
@@ -1,22 +1,23 @@
 // create a new module called 'myAppModule' and save 
-+// a reference to it in a variable called myAppModule 
-+var myAppModule = angular.module('caiwushouruchaxunModule', []);
-+
-+// use the myAppModule variable to
-+// configure the module with a controller
-+myAppModule.controller('MyFilterDemoCtrl', function ($scope) {
-+       // controller code would go here
-+      var Query options = {
-+            Income state: '未完成',
-+            Start date:  '2015-1-1',
-+     Termination date: '2015-2-13',
-+        };
-+        $scope.data =  Query options;
-+} 
-+
-+// use the myAppModule variable to
-+// configure the module with a filter
-+myAppModule.filter('stripDashes', function() {
-+    return function(txt) {
-+        // filter code would go here
-+}; }); 
+// a reference to it in a variable called myAppModule 
+var caiwushouruchaxun = angular.module('caiwushouruchaxun', []);
+
+// use the myAppModule variable to
+// configure the module with a controller
+caiwushouruchaxun.controller('MyFilterDemoCtrl', function ($scope) {
+      // controller code would go here
+      var Queryoptions = {
+            Incomestate:'未完成',
+            Startdate:'2015-1-1',
+            Terminationdate:'2015-2-13'
+        };
+        $scope.data =  Queryoptions;
+    }
+);
+
+// use the myAppModule variable to
+// configure the module with a filter
+caiwushouruchaxun.filter('stripDashes', function() {
+    return function(txt) {
+        // filter code would go here
+}; }); 
