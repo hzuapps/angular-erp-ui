39f9af0536a0dfd428d3fb509695ef980e2cb7c6
xiaoqiannba <229597145@qq.com>
2015-11-10 21:07:37 +0800

    实验五

--- works/1314080901235/js/itemListModule.js
+++ works/1314080901235/js/itemListModule.js
@@ -1,22 +1,31 @@
-// create a new module called 'myAppModule' and save 
+﻿// create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
 var itemListModule = angular.module('itemListModule', []);
 
 // use the myAppModule variable to
 // configure the module with a controller
 itemListModule.controller('MyFilterDemoCtrl', function ($scope) {
-       // controller code would go here
+          // controller code would go here
         var someDate = {
             code: '编码',
             name: '品名',
             type: '类型',
             color: '颜色',
-            inventory number: '库存数',
-            check number:'盘点数'
+            inventory_number: '库存数',
+            check_number:'盘点数'
         };
-        $scope.data = someDate;
+       
     }
 );
+
+itemListModule.directive('itemListModule',function(){
+console.log();
+	return {
+	restrict:'AE',
+	template:"<div ng-repeat=\"data in data\">"+
+			"{{data.code}}   &nbsp; &nbsp; &nbsp;  {{data.name}} &nbsp; &nbsp; &nbsp; {{data.type}} &nbsp; &nbsp; &nbsp;{{data.color}} &nbsp; &nbsp; &nbsp; {{data.inventory_number}} &nbsp; &nbsp; &nbsp; {{data.check_number}}</div>"
+};
+});
 
 // use the myAppModule variable to
 // configure the module with a filter
