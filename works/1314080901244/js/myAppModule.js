e50fd2aa608f5778cd9d117061a15042ea455ccb
YashonCheung <1446734764@qq.com>
2015-11-15 08:06:27 +0800

    实验四第一次提交
    更新index文件
    添加js文件

---
+++ works/1314080901244/js/myAppModule.js
@@ -0,0 +1,24 @@
+
+// create a new module called 'myAppModule' and save 
+// a reference to it in a variable called myAppModule 
+var myAppModule = angular.module('myAppModule', []);
+
+// use the myAppModule variable to
+// configure the module with a controller
+myAppModule.controller('MyFilterDemoCtrl', function ($scope) {
+       // controller code would go here
+      var someData={
+        Name:'新增工作组',
+        Detailed:'新增工作组组名',
+        instructions:'新建工作组',
+      };
+      $scope.data=someData;
+    }
+);
+
+// use the myAppModule variable to
+// configure the module with a filter
+myAppModule.filter('stripDashes', function() {
+    return function(txt) {
+        // filter code would go here
+}; });
