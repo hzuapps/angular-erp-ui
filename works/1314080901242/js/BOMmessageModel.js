5529428fcfce423aefd5fa6fd16bd0f7258aee5e
wszjx <1289245883@qq.com>
2015-11-16 13:53:17 +0800

    第五次实验

--- works/1314080901242/js/BOMmessageModel.js
+++ works/1314080901242/js/BOMmessageModel.js
@@ -1,23 +1,46 @@
- <!DOCTYPE html>
+
 // create a new module called 'BOMmessageModule' and save 
 // a reference to it in a variable called BOMmessageModule 
 var BOMmessageModule = angular.module('BOMmessageModule', []);
 
 // use the BOMmessageModule variable to
 // configure the module with a controller
-orderModule.controller('createBOMmessageCtrl', function($scope) {
+BOMmessageModule.controller('createBOMmessageCtrl', function($scope) {
 	var BOMmessage = {
-	  dingdan: '某某公司装修工程',
+	        dingdan: '某某公司装修工程',
           chanping: '楼层装饰',
           data1: '2015.9.1',
           data2: '2015.9.31',
-          status: '装修中',
+          status: 'zhuangxiuzhong',
           caidanyuan: '小张',
-          beizhu: '无',
+          beizhu: '无'
 	};
 	$scope.data = BOMmessage;
 });
-
+BOMmessageModule.directive('gongchengList', function ($compile) {
+return {
+        restrict: 'AE',
+        template: "<button ng-click='showHideColors()' type='button'>"
+            + "{{isHidden ? 'Show Available Colors' : 'Hide Available Colors'}}"
+            + "</button><div ng-hide='isHidden' id='colorContainer'>"
+            + "</div>"
+            +"<div>"
+            + "{{data.data1}}</div>",
+        link: function ($scope, $element) {
+            // set default state of hide/show
+            $scope.isHidden = true;
+            // add function to manage hide/show state
+            $scope.showHideColors = function () {
+                $scope.isHidden = !$scope.isHidden;
+            }
+      // add colors divs to the document
+                var colorContainer = $element.find('div'); angular.forEach($scope.colorsArray, function (color) {
+                var appendString = "<div style='background-color:" + color + "'>" + color +   "</"
+                colorContainer.append(appendString);
+            });
+    } 
+  };
+});
 // use the BOMmessageModule variable to
 // configure the module with a filter
 BOMmessageModule.filter('stripDashes', function() {
