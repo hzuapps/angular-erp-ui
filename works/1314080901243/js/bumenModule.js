6f465784cd976402d4264a9efca971efe6fdaca6
xiaoyaobrother <392205128@qq.com>
2015-11-17 21:46:46 +0800

    实验五

--- works/1314080901243/js/bumenModule.js
+++ works/1314080901243/js/bumenModule.js
@@ -1,18 +1,18 @@
-// create a new module called 'myAppModule' and save 
+// create a new module called 'bumenModule' and save 
 // a reference to it in a variable called myAppModule 
-var myAppModule = angular.module('myAppModule', []);
+var bumenModule = angular.module('bumenModule', []);
 
 // use the myAppModule variable to
 // configure the module with a controller
-myAppModule.controller('MyFilterDemoCtrl', function ($scope) {
+bumenModule.controller('MyFilterDemoCtrl', function ($scope) {
     // controller code would go here
-        var someData = {
+        var bumenData = {
             bumenname:'部门名称',
             bumendaima:'部门代码',
             bumenzhuguan:'部门主管',
-            bumenmiaosu:'部门描述'
+            bumenmiaoshu:'部门描述'
         };
-        $scope.data = someData;
+        $scope.data = bumenData;
     }
 );
 
