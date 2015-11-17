19a3fdbf4c9fd7fb3010e4cbdfc9152486ee73f0
zengyt <1781262224@qq.com>
2015-11-17 09:17:37 +0800

    2班01号 实验五

--- works/1314080901201/js/myMaterialsModule.js
+++ works/1314080901201/js/myMaterialsModule.js
@@ -3,23 +3,27 @@
 // a reference to it in a variable called myMaterialsModule 
 var myMaterialsModule = angular.module('myMaterialsModule', []);
 
-myMaterialsModule.controller('createmyMaterialsCtrl', function ($scope) {
+ myMaterialsModule.controller('createmyMaterialsCtrl', function ($scope) {
         var someData = {
             number:100,
             date:'建档日期',
             sort:'供应商类别',
-            name:'供应商全称',
+            fullname:'供应商全称',
             code:'拼音码',
             homepage:'供应商主页',
-            jc:'供应商简称',
+            simplename:'供应商简称',
             area:'行政区域',
-            synposis:'供应商简介',
-            dateJoined: new Date(2015, 10, 20),
+            synposis:'供应商简介'
         };
         $scope.data = someData;
+        $scope.isHidden=true;
+        $scope.showInput=function(){
+        	$scope.isHidden=!$scope.isHidden;
+        }
 });
+ 
  myMaterialsModule.filter('stripDashes', function() {
     return function(txt) {
         // filter code would go here
-}; });
-\ No newline at end of file
+}; 
+});
