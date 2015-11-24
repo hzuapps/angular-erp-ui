0b81a37bf337e3378262fa2938a6e738f61cdf9f
hzukondy <493030390@qq.com>
2015-11-24 15:16:06 +0800

    实验五

--- works/1314080901128/js/workers.js
+++ works/1314080901128/js/workers.js
@@ -5,15 +5,36 @@ var myAppModule = angular.module('workersModule', []);
 // use the myAppModule variable to
 // configure the module with a controller
 myAppModule.controller('MyFilterDemoCtrl', function ($scope) {
-	 var workers = {
-            no: '1',
-            worker_no:'1',
-            name:'chenzhengang',
-            sex:'f',
-            id_card:'4415210000000000001234',
-            address:'gd',
-            phone_number:'13888888888',
-            post:'manager'
+	 var someData = {
+            "workers":[
+            {
+            "number": '1',
+            "worker_no":'201501',
+            "name":'chenzhengang',
+            "sex":'f',
+            "id_card":'4415210000000000001234',
+            "address":'gd',
+            "phone_number":'13888888881',
+            "post":'manager1'},
+            {
+            "number": '2',
+            "worker_no":'201502',
+            "name":'bcd',
+            "sex":'f',
+            "id_card":'4415210000000000001235',
+            "address":'gd',
+            "phone_number":'13888888882',
+            "post":'manager'},
+            {
+            "number": '3',
+            "worker_no":'201503',
+            "name":'abc',
+            "sex":'m',
+            "id_card":'4415210000000000001236',
+            "address":'gd',
+            "phone_number":'13888888883',
+            "post":'woker'}
+            ]
         };
         $scope.data = someData;
        // controller code would go here
