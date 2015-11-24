f7a4801563c0858571ad159c6c24aecdbc8c94dc
SimpleCodeCX <248200851@qq.com>
2015-11-24 10:33:04 +0800

    实验5-使用Directives渲染列表数据

--- works/1314080901208/js/myOrderModule.js
+++ works/1314080901208/js/myOrderModule.js
@@ -1,5 +1,6 @@
 
-//¶¨Òå±íµ¥Êý¾ÝÊµÌå
+
+//定义表单数据实体
       var orderData = {
           orderId: 1,
           storage: 'myStorage',
@@ -14,6 +15,7 @@
 
 //
 var myOrderModule = angular.module("OrderModule", []);
+
 myOrderModule.controller("CreateOrderInformation", function ($scope) {
     
    
@@ -21,14 +23,28 @@ myOrderModule.controller("CreateOrderInformation", function ($scope) {
     orderData1.orderId = 1;
     orderData1.storage = "storage1";
     orderData1.people = "simple";
-    $scope.theOrderDatas = [orderData1];
+    $scope.theOrderDatas = [orderData];
 });
+
+myOrderModule.directive("myorderdata",function(){
+  return{
+      restrict:"AEC",
+      template:'<div id="tableBody-data" style="margin-top:5px;" ng-repeat="thedata in theOrderDatas">'
+                 +'<div id="orderId" class="order orderId" style="">{{thedata.orderId}}</div>'
+                 +'<div id="storage" class="order storage" style="">{{thedata.storage}}</div>'
+                 +'<div id="people" class="order people" style="">{{thedata.people}}</div>'
+                 +'<div id="dateTime" class="order dateTime" style="">{{thedata.dateTime}}</div>'
+                 +'<div id="status" class="order status" style="">{{thedata.status}}</div>'
+                 +'<div id="notes" class="order notes" style="">{{thedata.notes}}</div>'
+                 +'</div>',
+      transclude: true
+  };
+});
+
 myOrderModule.filter("", function () {
-    
-
 });
 
 
 
-//²âÊÔÒýÓÃjsÎÄ¼þÊÇ·ñ³É¹¦µÄº¯Êýµ÷ÓÃ
-function a() { alert("ÒýÓÃjsÎÄ¼þ³É¹¦");}
+//测试引用js文件是否成功的函数调用
+function a() { alert("引用js文件成功");}
