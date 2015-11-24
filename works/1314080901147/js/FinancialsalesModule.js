34ad9693eec3421d4b8aba20911a5b8ab58c256f
mumuLOVEanni <245189703@qq.com>
2015-11-24 10:52:52 +0800

    实验四

---
+++ works/1314080901147/js/FinancialsalesModule.js
@@ -0,0 +1,27 @@
+// create a new module called 'FinancialsalesModule' and save 
+// a reference to it in a variable called FinancialsalesModule
+var FinancialsalesModule = angular.module('FinancialsalesModule', []);
+
+// use the FinancialsalesModule variable to
+// configure the module with a controller
+FinancialsalesModule.controller('MyfilterDemoCtrl', function ($scope) {
+       // controller code would go here
+        var Financialsales1 = {
+            orderNumber:'123',
+            ordername:'123',
+            summary:'abc',
+            moneyed:'123',
+            money:'123',
+            beizhu:'abc',
+            
+        };
+        $scope.data = Financialsales1;
+    }
+);
+
+// use the Financialsales variable to
+// configure the module with a filter
+FinancialsalesModule.filter('stripDashes', function() {
+    return function(txt) {
+        // filter code would go here
+}; });
