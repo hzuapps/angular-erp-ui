1fef90a8fd920ddaf885bba25aabe35ba1f5144c
renrendashai <297259225@qq.com>
2015-12-01 18:21:10 +0800

    修改后的实验四

---
+++ works/1314080901109/js/myAppModule.js
@@ -0,0 +1,31 @@
+// create a new module called 'myAppModule' and save 
+// a reference to it in a variable called myAppModule 
+var myAppModule = angular.module('supplierInformationModule', []);
+
+// use the myAppModule variable to
+// configure the module with a controller
+myAppModule.controller('MyFilterDemoCtrl', function ($scope) {
+	
+        var Danjumingxi = {
+            XUHAO: '01',
+            BIANMA: '1008611',
+            LEIBIE: 'watch',
+            NAME: 'SEIKO,
+            XINGHAO: '1008612'
+            DANWEI: '100',
+            SHULIANG: '1000',
+            SHOUHUOSHULIANG: '998',
+            BEIZHU: 'NOTHING',
+        
+        };
+        $scope.data = someData;
+       // controller code would go here
+    }
+);
+
+// use the myAppModule variable to
+// configure the module with a filter
+myAppModule.filter('stripDashes', function() {
+    return function(txt) {
+        // filter code would go here
+}; });
