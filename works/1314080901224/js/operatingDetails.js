595ea95967e6e50ad5433c14c969720baf6f9968
linxingwei <1650378792@qq.com>
2015-12-03 18:00:32 +0800

    实验5

--- works/1314080901224/js/operatingDetails.js
+++ works/1314080901224/js/operatingDetails.js
@@ -13,11 +13,49 @@ var operationData = {
 
 
 
+//定义控制器
 var myOperationModule=angular.module("operationModule",[]);
 myOperationModule.controller("CreateOperation",function($scope){
 $scope.theData=operationData;
 
 });
+
+
+//实用derective
+//操作名
+myOperationModule.directive("thename",function(){
+return{
+      restrict:"AEC",
+      template:'<input id="operationName" type="text" style="width:200px;height:30px;" ng-model="theData.operateName">',
+      replace:true
+
+};
+});
+
+//操作明细
+myOperationModule.directive("thedetail",function(){
+return{
+      restrict:"AEC",
+      template:'<textarea id="operationDetail" style="width:400px;height:100px;" ng-model="theData.detail"></textarea>',
+      replace:true
+
+};
+});
+
+//操作批示
+myOperationModule.directive("thenotes",function(){
+return{
+      restrict:"AEC",
+      template:'<textarea id="operationNotes" style="width:500px;height:100px;" ng-model="theData.notes"></textarea>',
+      replace:true
+
+};
+});
+
+
+
+
+
 
 
 
