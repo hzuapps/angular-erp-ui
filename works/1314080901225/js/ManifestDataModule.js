12ad09fb12e5bf2cee0eddfb04110832b37f934e
qingtian1993 <15767979110@163.com>
2015-12-08 09:36:09 +0800

    创建service并获取json文件

--- works/1314080901225/js/ManifestDataModule.js
+++ works/1314080901225/js/ManifestDataModule.js
@@ -1,36 +1,92 @@
-// create a new module called 'myAppModule' and save 
-// a reference to it in a variable called myAppModule 
+
+//初始化表单数据
+var ManifestData = {
+		ManifestNum: '001',
+        Wearhouse: '001',
+        Handler: 'Ayden',
+        HandlingData: '2000-10-15',
+        State: ' ',
+	    Remark: ' ',
+		
+
+};
+
+
+//创建模块
 var ManifestDataModule = angular.module('ManifestDataModule', []);
 
-// use the myAppModule variable to
-// configure the module with a controller
-angular.module('ManifestDataModule').controller('MyFilterDemoCtrl', function($scope) {
+//创建服务
+ManifestDataModule.factory("ManifestService",function($http){
+      var getManifestData={};
       
-        	$scope.isCreat=true;
-		$scope.showCreat=function(){
-			$scope.isCreat=!$scope.isCreat;
-		}
-
-		$scope.isSave=true;
-		$scope.showSave=function(){
-			$scope.isSave=!$scope.isSave;
-		}
-			
-		var ManifestData = {
-            	ManifestNum: '001',
-            	Wearhouse: '001',
-            	Handler: 'Ayden',
-            	HandlingData: '2000-10-15',
-            	State: ' ',
-	    	Remark: ' ',
-            
-       		};
-        	$scope.data =ManifestData;
+	  //getData方法
+      getManifestData.getData=function(buttondata){
+            var promise=$http({
+              url:"jsonData/buttonData.json",
+              method:"GET",
+			  data:buttondata
+            });
+            return promise;
+      }
+      
+      return getManifestData;
 
 });
 
-// use the myAppModule variable to
-// configure the module with a filter
+//定义控制器
+
+angular.module('ManifestDataModule').controller('MyFilterDemoCtrl', function($scope,ManifestService) {
+          var promise=ManifestService.getData();
+		  
+		  $scope.create = function () {
+			$scope.showSuccessMessage=false;
+			$scope.showErrorMessage=false;
+			//promise的成功方法
+			promise.success(function(data,status){
+			
+		  
+			$scope.successMessage =data[0].SMessage;
+   		    $scope.showSuccessMessage=true;
+			});
+		  
+			//promise的失败方法
+			promise.error(function(data,status){
+		  
+		  
+			$scope.errorsMessage =data[0].EMessage;
+			$scope.showErrorMessage=true; 
+		  
+			});
+		  }
+
+			
+			
+			$scope.SelectItem = [
+			 {
+				ManifestNum:'00001',
+				Wearhouse:'1号仓库',
+				Handler:'富土康质检员张全蛋',
+
+			 },
+			 {
+				ManifestNum:'00002',
+				Wearhouse:'2号仓库',
+				Handler:'隔壁老王',
+			 }
+			 
+			];
+			
+			
+			
+
+			
+
+
+});
+
+
+
+//定义过滤器
 ManifestDataModule.filter('stripDashes', function() {
 	return function(txt) {
 		// filter code would go here
