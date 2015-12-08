8d8ab86d4bf50f442232eaae944ba793f27fa980
DUDUfine <1607778925@qq.com>
2015-12-08 08:31:05 +0800

    实验六
    服务

--- works/1314080901104/js/rejectRequestModule.js
+++ works/1314080901104/js/rejectRequestModule.js
@@ -4,28 +4,52 @@ var rejectRequestModule = angular.module('rejectRequestModule', []);
 
 // use the myAppModule variable to
 // configure the module with a controller
-rejectRequestModule.controller('MyFilterDemoCtrl', function ($scope) {
-       // controller code would go here
-    var someData = {
+
+
+rejectRequestModule.factory('providerService',function($http){
+  var dataSvc={};
+  dataSvc.getData=function(theData){
+    var promise=$http({method: 'POST',url: 'json/message.json',data: theData});
+    return promise;
+  }
+  return dataSvc;
+}).controller("MyFilterDemoCtrl",function($scope,providerService){
+          var someData = {
               name: 'storage',
               detailed: 'Increase 100 pieces of goods',
               instructions: 'Approval',
               };
-    $scope.data = someData;
-    $scope.isHidden = true;
-    $scope.showInput = function () {
-    $scope.isHidden = !$scope.isHidden;
-       }
-       $scope.showTextarea = function () {
-    $scope.isHidden = !$scope.isHidden;
-       }
-  }
-);
-
-// use the myAppModule variable to
-// configure the module with a filter
-rejectRequestModule.filter('stripDashes', function() {
-    return function(txt) {
-        // filter code would go here
-    }; 
+          $scope.data=someData;
+          $scope.isHidden=true;
+          $scope.showInput = function () {
+              $scope.isHidden = !$scope.isHidden;
+          }
+          $scope.showTextarea = function () {
+              $scope.isHidden = !$scope.isHidden;
+          }
+              
+          // $scope.provider={};
+          $scope.register=function(){
+              var promise=providerService.getData($scope.provider);
+              promise.success(function(data,status,headers,config,statusText){
+                  $scope.backMess=data.success;
+                  $scope.isHidden = !$scope.isHidden;
+                  if (!$scope.isHidden) {
+                    alert($scope.backMess[0].message+"\n"+"\n"+"The name is "+$scope.name);
+                  }
+                });
+                promise.error(function(data,status,headers,config,statusText){
+                    $scope.backMess=data.error;
+                    $scope.isHidden = !$scope.isHidden;
+                    if (!$scope.isHidden) {
+                        alert($scope.backMess[0].message);
+                    }
+                });
+            }
 });
+
+
+rejectRequestModule.filter('stripDashes', function() {
+     return function(txt) {
+         // filter code would go here
+ }; });
