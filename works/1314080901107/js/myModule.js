ca0d10175694f870c347f2732c0bea9269b94339
chensongkui <625246330@qq.com>
2015-12-10 00:54:02 +0800

    实验5 from chensongkui

--- works/1314080901107/js/myModule.js
+++ works/1314080901107/js/myModule.js
@@ -1,6 +1,6 @@
  
 var myapp = angular.module("myapp", []);
-+myapp.controller("homeController", function ($scope){
+myapp.controller("homeController", function ($scope){
 	var renshizz = {
             suoshubumen: 'renshi',
             gangweimingcheng: 'GRANT',
@@ -11,3 +11,58 @@ var myapp = angular.module("myapp", []);
         };
 		$scope.renshizz1 = renshizz;
 });
+
+myapp.directive("thesuoshubumen",function(){
+return{
+     restrict:"AEC",
+     template:'<input type="text" style="margin-left:5px;" ng-model="renshizz1.suoshubumen">',
+      replace:true
+
+};
+});
+
+myapp.directive("thegangweimingcheng",function(){
+return{
+     restrict:"AEC",
+     template:'<input type="text" style="margin-left:5px;" ng-model="renshizz1.gangweimingcheng">',
+      replace:true
+
+};
+});
+
+myapp.directive("thegangweichenchi",function(){
+return{
+     restrict:"AEC",
+     template:'<input type="text" style="margin-left:5px;" ng-model="renshizz1.gangweichenchi">',
+      replace:true
+
+};
+});
+
+myapp.directive("thegangweileixin",function(){
+return{
+     restrict:"AEC",
+     template:'<input type="text" style="margin-left:5px;" ng-model="renshizz1.gangweileixin">',
+      replace:true
+
+};
+});
+
+myapp.directive("thejibenxinzhi",function(){
+return{
+     restrict:"AEC",
+     template:'<input type="text" style="margin-left:5px;" ng-model="renshizz1.jibenxinzhi">',
+      replace:true
+
+};
+});
+
+myapp.directive("thegangweizhizhe",function(){
+return{
+     restrict:"AEC",
+     template:'<input type="text" style="margin-left:5px;" ng-model="renshizz1.gangweizhizhe">',
+      replace:true
+
+};
+});
+
