a55b438b0dc173de1912ef436a5e148fdab11602
Robin30517 <zhcbin520@qq.com>
2015-12-10 10:31:55 +0800

    实验六（1314080901241）

--- works/1314080901241/js/robinModule.js
+++ works/1314080901241/js/robinModule.js
@@ -1,37 +1,17 @@
-<<<<<<< HEAD
-    // create a new module called 'myAppModule' and save 
+ // create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
 var robinModule = angular.module('robinModule', []);
-=======
-// create a new module called 'myAppModule' and save 
-// a reference to it in a variable called myAppModule 
-var myAppModule = angular.module('robinModule', []);
->>>>>>> origin/master
 
 // use the myAppModule variable to
 // configure the module with a controller
-robinModule.controller('createDemoCtrl', function ($scope) {
-       // controller code would go here
-<<<<<<< HEAD
-        $scope.someData = [
-           { type: '1',name: '内侧板',description:'结实'},
-           { type: '3',name: '外侧板',description:'美观'},
-           { type: '2',name: '中侧板',description:'好用'}
-           ];
-=======
 
-  	  
-  	      var DataList = {
-  	          classification: '类别',
-  	          name: '名称',
-   	          description:'描述',
-    		  };
-       	 $scope.data = DataList;
-		} 
-	
->>>>>>> origin/master
-    }
-);
+robinModule.controller('createDemoCtrl', function ($http,$scope) {
+       // controller code would go here
+       $http.get("bjList_JSON.txt").success(function(someData){
+        $scope.data=someData;
+       });
+     })
+
 
 // use the myAppModule variable to
 // configure the module with a filter
