e70542bd5eb56a3341f5378491fabc429197630d
thegoodboy1234 <1019389480@qq.com>
2015-12-15 21:54:54 +0800

    实验7

--- works/1314080901118/js/myAppModule.js
+++ works/1314080901118/js/myAppModule.js
@@ -1,7 +1,29 @@
 // create a new module called 'myAppModule' & save 
 // a reference to it in a variable called myAppModule 
-var myAppModule = angular.module('myAppModule', []);
-
+var myAppModule = angular.module('myAppModule', ['ngRoute']);
+myAppModule.config(function ($routeProvider) {
+    // configure the routes
+    $routeProvider
+        .when('/', {
+            // route for the about page
+            templateUrl: 'pages/home.html'
+        })
+      .when('/home', {
+           templateUrl: 'pages/home.html'
+        })
+        .when('/input', {
+            templateUrl: 'pages/input.html',
+            controller: 'inputCtrl'
+        })
+        .when('/about', {
+            templateUrl: 'pages/about.html',
+            controller: 'aboutCtrl'
+        })
+        .when('/content', {
+            templateUrl: 'pages/content.html',
+            controller: 'contentCtrl'
+        });
+});
 // use the myAppModule variable to
 // configure the module with a controller
 myAppModule.factory('providerService', function ($http) 
@@ -13,9 +35,11 @@ myAppModule.factory('providerService', function ($http)
             return promise;
         }
         return dataStroe;
-    }).controller('MyFilter', function ($scope,providerService) {
+    });
 
-        var PROData = 
+myAppModule.controller('inputCtrl', function ($scope, providerService){
+
+        $scope.providerArray = 
       {
             Pno: 't0001',
             Sort: 'coms',
@@ -26,7 +50,6 @@ myAppModule.factory('providerService', function ($http)
             Num:0,
             Other:' '
         };
-        $scope.data = PROData;
     $scope.isHidden = true;
     $scope.showInput  = function () {
     $scope.isHidden = !$scope.isHidden;
@@ -44,15 +67,27 @@ myAppModule.factory('providerService', function ($http)
                 }
             });
             promise.error(function (data, status, headers, config, statusText) {
-                $scope.backMess = data.error;
-                $scope.isHidden = !$scope.isHidden;
-                if(!$scope.isHidden){
-                    alert($scope.backMess[0].message);
-                }
+                $scope.backMess = '出了问题，请重新提交！';
+                alert($scope.backMess);
             });
         } 
-    });
-
+}).controller('aboutCtrl', function ($scope) {
+    var aboutData = {
+        num: '13 计 1 18'
+    };
+        $scope.aboutData = aboutData;
+}).controller('contentCtrl', function ($scope) {
+    $scope.providerArray = [{
+            Pno: 't0001',
+            Sort: 'coms',
+            Name: '锁存器',
+            Size: '74ls273',
+            Color: '黑',
+            Unit:' ',
+            Num:0,
+            Other:' '
+    }];
+});
 // use the myAppModule variable to
 // configure the module with a filter
 myAppModule.filter('stripDashes', function() {
@@ -60,4 +95,30 @@ myAppModule.filter('stripDashes', function() {
         // filter code would go here
     };
 
+});
+myAppModule.directive("myProvider", function () {
+    return {
+        restrict: "AE",
+        replace: true,
+        templateUrl: 'directives/provider.html'
+
+    }
+}).directive("myHide", function () {
+    return {
+        restrict: "AE",
+        replace: true,
+        templateUrl: 'directives/hide.html'
+    }
+}).directive("myNav", function () {
+    return {
+        restrict: "AE",
+        replace: true,
+        templateUrl: 'directives/nav.html'
+    }
+}).directive("myShow", function () {
+    return {
+        restrict: "AE",
+        replace: true,
+        template: '<div id="main"><div ng-view></div></div>'
+    }
 });
