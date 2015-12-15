cde169bc74a36a0f12ae708beec075849ac16745
renrendashai <297259225@qq.com>
2015-12-15 15:15:31 +0800

    实验六

--- works/1314080901109/js/myAppModule.js
+++ works/1314080901109/js/myAppModule.js
@@ -1,12 +1,19 @@
 // create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
 var myAppModule = angular.module('myAPPModule', []);
-
+myAppModule.factory('myAppService',function($http){
+ 	    var dataStroe = {};
+         dataStroe.doRegistration = function (theData) {
+             var promise = $http({method: 'POST',url: 'json/message.json',data: theData});
+             return promise;
+         }
+         return dataStroe;
+     }).controller('MyFilterDemoCtrl', function ($scope,myAppService) {
+	
 // use the myAppModule variable to
 // configure the module with a controller
-myAppModule.controller('MyFilterDemoCtrl', function ($scope) {
-	
-        var Danjumingxi = {
+
+        var DanjumingxiData = {
             XUHAO: '01',
             BIANMA: '1008611',
             LEIBIE: 'watch',
@@ -18,16 +25,36 @@ myAppModule.controller('MyFilterDemoCtrl', function ($scope) {
             BEIZHU: 'NOTHING',
         
         };
-        $scope.data = someData;
+        $scope.data = DanjumingxiData;
        // controller code would go here
       $scope.isHidden = true;
       $scope.showHideColors = function () {
            $scope.isHidden = !$scope.isHidden;
     }
 
+        $scope.provider = {};
+          $scope.register = function () {
+             var promise = providerService.doRegistration($scope.provider);
+                promise.success(function (data, status, headers, config, statusText) {
+                   $scope.backMess = data.success;
+                        $scope.isHidden = !$scope.isHidden;
+        
+        if(!$scope.isHidden){
+                   alert($scope.backMess[0].message + "\n" + "\n" + "ÄãµÄ²Ö¿âµ¥ºÅÊÇ£º " + $scope.Pno);
+                }
 
-    }
-);
+    });
+    promise.error(function (data, status, headers, config, statusText) {
+                $scope.backMess = data.error;
+                    $scope.isHidden = !$scope.isHidden;
+                        if(!$scope.isHidden){
+                           alert($scope.backMess[0].message);
+                  } 
+            });
+        } 
+    });
+
+
 
 // use the myAppModule variable to
 // configure the module with a filter
