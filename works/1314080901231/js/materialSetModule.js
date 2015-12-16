1c9d33e637f927e10b6c392fe47f17e895d645d9
fanrenaren <fanrenaren608@163.com>
2015-12-16 16:21:24 +0800

    第五次实验修改
    初步显示界面

---
+++ works/1314080901231/js/materialSetModule.js
@@ -0,0 +1,32 @@
+﻿// create a new module called 'materialsSetModule' and save 
+// a reference to it in a variable called itemListModule
+ var materialSetModule = angular.module('materialSetModule', []);
+ 
+// use the materialSetModule variable to
+// configure the module with a controller
+materialSetModule.controller('materialSetCtrl', function ($scope)
+    {
+        var materialSet = {
+                typeName: '板材',
+                typeCode: 'bc'
+            };
+			
+            $scope.data = materialSet;
+			
+            $scope.items = [
+				{ typeName: '板材',typeCode: 'bc'},
+				{ typeName: '五金',typeCode: 'wj'},
+				{ typeName: '木皮',typeCode: 'mp'},
+				{ typeName: '灯具',typeCode: 'dj'},
+				{ typeName: '油漆',typeCode: 'yq'},
+				{ typeName: '铰链 ',typeCode: 'jl'}
+             ];
+    }
+);
+ 
+ // use the materialSetModule variable to
+ // configure the module with a filter
+materialSetModule.filter('materialSetstripDashes', function() {
+     return function(txt) {
+         // filter code would go here
+    }; });
