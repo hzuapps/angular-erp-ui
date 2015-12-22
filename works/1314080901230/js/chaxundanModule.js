aac22b82773527dae58b5de29c12903f8b4a7991
30-wangweixin <498837428@qq.com>
2015-12-22 08:50:43 +0800

    Update chaxundanModule.js

--- works/1314080901230/js/chaxundanModule.js
+++ works/1314080901230/js/chaxundanModule.js
@@ -6,28 +6,23 @@
 	// configure the module with a controller
 	chaxundanModule.controller('MyFilterDemoCtrl', function ($scope) {
 	       // controller code would go here
-	 {
-	
-	
-	    $scope.isHidden = true;
-	    $scope.showHideColors = function () {
-	    $scope.isHidden = !$scope.isHidden;
-	    }
-	        var someData = 
-	      {
-	Unit name:"wwx",
-	         Payment method:"支付宝",
-	         Payment date:"2015-10-20",
-	         Middle man:"wwx",
-	         Payment amount:"wwx",
-	         Payment account:"wwx",
-	         Bank account:"wwx",
-	         State:"wwx",
-	        };
-	        $scope.data = someData;
-	} 
-	    }
-	);
+	     var chaxundanData = {
+	         Unit name:'wwx',
+	         Payment method:'支付宝',
+	         Payment date:'2015-10-20',
+	         Middle man:'wwx',
+	         Payment amount:'wwx',
+	         Payment account:'wwx',
+	         Bank account:'wwx',
+	         State:'wwx',
+	       };
+	         $scope.data = chaxundanData;
+                 $scope.isHidden = true;
+                 $scope.showInput = function () {
+                      $scope.isHidden = !$scope.isHidden;
+        }
+}
+);
 	
 	// use the chaxundanModule variable to
 	// configure the module with a filter
