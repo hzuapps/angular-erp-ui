397a58c1df75ce989b49ac6970e553ee9841f0c0
finalnbn <122250139@qq.com>
2015-12-22 08:27:48 +0800

    实验六

--- works/1314080901134/js/purchasingGroupDelModule.js
+++ works/1314080901134/js/purchasingGroupDelModule.js
@@ -1,37 +1,28 @@
 // create a new module called 'purchasingGroupDelModule' and save 
 // a reference to it in a variable called purchasingGroupDelModule 
-var purchasingGroup1 ={
-	groupName:'Name',
-	groupID  :'Id'
-};
-var purchasingGroupDelModule = angular.module('purchasingGroupDelModule', []);
-
+var purchasingGroupDelModule = angular.module('purchasingGroupDelModule', ['ngRoute']);
+purchasingGroupDelModule.config(function ($routeProvider){
+  $routeProvider
+      .when('/about',{
+        templateUrl:'about.html'
+      });
+});
 // use the purchasingGroupDelModule variable to
 // configure the module with a controller
 purchasingGroupDelModule.factory("purchasingGroupService",function($http){
-	var getpurchasingGroupData={};
-	getpurchasingGroupData.getData=function(){
-		var promise=$http({
-			url:"json/purchasingGroup1.json",
-			method:"get",
-		});
-		return promise;
-	}
-	return getpurchasingGroupData;
-
-})
-purchasingGroupDelModule.controller('purchasingGroupDelCtrl', function ($scope,purchasingGroupService) {
+  var getpurchasingGroupData={};
+  getpurchasingGroupData.getData=function(){
+      var promise = $http({method: 'GET',url: 'json/purchasingGroup1.json'});
+      return promise;
+  }
+  return getpurchasingGroupData;
+}).controller('purchasingGroupDelCtrl', function ($scope,purchasingGroupService) {
        // controller code would go here
-         var promise=purchasingGroupService.getData();
+      var promise=purchasingGroupService.getData();
       promise.success(function(data,status){
-      	var purchasingGroupdata1=purchasingGroup1;
-      	purchasingGroupdata1.groupName=data["groupName"];
-      	purchasingGroupdata1.groupID=data["groupID"];
-         $scope.data =purchasingGroupdata1; 
-      }
-);
-    }
-);
+        $scope.messages = data.purchasingGroup1; 
+      });
+});
 
 // use the purchasingGroupDelModule variable to
 // configure the module with a filter
@@ -40,11 +31,3 @@ purchasingGroupDelModule.filter('stripDashes', function() {
         // filter code would go here
 }; });
 
-purchasingGroupDelModule.config(function ($routeProvider) {
-  // configure the routes
-  $routeProvider
-    // route for the about page
-        .when('/about', {
-          templateUrl: 'pages/about.html'
-        });
-    });
