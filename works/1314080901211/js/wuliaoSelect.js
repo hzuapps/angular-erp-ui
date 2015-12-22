ac114eaff401e2c9a87dc79fd0684bc93a34995f
berrychong <1090014784@qq.com>
2015-12-22 15:29:43 +0800

    实验七

--- works/1314080901211/js/wuliaoSelect.js
+++ works/1314080901211/js/wuliaoSelect.js
@@ -1,16 +1,38 @@
     // create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
-var wuliaoSelect = angular.module('wuliaoSelect', []);
+var wuliaoSelect = angular.module('wuliaoSelect', ['ngRoute']);
 
 // use the myAppModule variable to
 // configure the module with a controller
+ wuliaoSelect.config(function ($routeProvider) {
+      // configure the routes
+    $routeProvider
+    .when('/index', {
+      // route for the home page
+        templateUrl: 'title.html',
+    })
+    .when('/home', {
+      // route for the home page
+        templateUrl: 'home.html',
+        controller: 'homeController'
+    })
+    .when('/about', {
+      // route for the about page
+        templateUrl:'about.html',
+        controller:'aboutController'
+    });
+});
 
-wuliaoSelect.controller('wuliaoSelectCtrl', function ($http,$scope) {
+wuliaoSelect.controller('aboutController', function ($scope) {
+      $scope.title = ' 学号1314080901211';
+    });
+
+
+wuliaoSelect.controller('homeController', function ($http,$scope) {
        // controller code would go here
-       $http.get("materialList_JSON.txt").success(function(someData){
-        $scope.data=someData;
-       });
-     })
+       $http.get("materialList_JSON.txt").success(function(someData){$scope.data=someData;});
+     
+    });
 
 
 // use the myAppModule variable to
