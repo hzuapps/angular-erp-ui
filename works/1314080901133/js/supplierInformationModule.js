2f6f5f1ebf90d9ae7a324dbb6a3b12b7787b4301
Wanghengdan <452950236@qq.com>
2015-12-22 13:51:02 +0800

    实验六
    sever

--- works/1314080901133/js/supplierInformationModule.js
+++ works/1314080901133/js/supplierInformationModule.js
@@ -4,40 +4,13 @@ var supplierInformationModule = angular.module('supplierInformationModule', []);
 
 // use the supplierInformationModule variable to
 // configure the module with a controller
-angular.module('supplierInformationModule').controller('MyFilterDemoCtrl', function($scope) {
 
-    $scope.isHidden = true;
-    $scope.showHideColors = function() {
-        $scope.isHidden = !$scope.isHidden;
-    }
-
-    var someData = {
-        "suppliers": [{
-            numbers: '131401001',
-            name: 'xidada1',
-            code: '110120119',
-            photo: '20151021.jpg',
-            createTime: '2015-10-21'
-        }, {
-            numbers: '131401002',
-            name: 'xidada2',
-            code: '110120120',
-            photo: '20151022.jpg',
-            createTime: '2015-10-22'
-        }, {
-            numbers: '131401003',
-            name: 'xidada3',
-            code: '110120121',
-            photo: '20151023.jpg',
-            createTime: '2015-10-23'
-        }]
-
-    };
-    $scope.data = someData;
-    // controller code would go here
-
-
-});
+supplierInformationModule.controller('MyFilterDemoCtrl', function ($http,$scope) {
+       // controller code would go here
+       $http.get("supplierInformationList_JSON.txt").success(function(someData){
+        $scope.data=someData;
+       });
+     })
 
 // use the supplierInformationModule variable to
 // configure the module with a filter
