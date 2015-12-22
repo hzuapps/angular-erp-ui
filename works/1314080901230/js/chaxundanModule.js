47f6d896e7074932d2637aed1c18a687290e273b
30-wangweixin <498837428@qq.com>
2015-12-22 09:34:08 +0800

    第六次实验

--- works/1314080901230/js/chaxundanModule.js
+++ works/1314080901230/js/chaxundanModule.js
@@ -4,30 +4,55 @@
 	
 	// use the chaxundanModule variable to
 	// configure the module with a controller
-	chaxundanModule.controller('MyFilterDemoCtrl', function ($scope) {
+	chaxundanModule.factory('chaxundanService',function($http){
+		 var dataStroe = {};
+                 dataStroe.doRegistration = function (theData) {
+                 var promise = $http({method: 'POST',url: 'json/message.json',data: theData});
+                 return promise;
+        }
+        return dataStroe;
+    }).controller('MyFilter', function ($scope,chaxundanService) {
 	       // controller code would go here
 	     var chaxundanData = {
-	         Unit name:'wwx',
-	         Payment method:'支付宝',
-	         Payment date:'2015-10-20',
-	         Middle man:'wwx',
-	         Payment amount:'wwx',
-	         Payment account:'wwx',
-	         Bank account:'wwx',
-	         State:'wwx',
+	         chaxundanUnitname:'wwx',
+	         chaxundanPaymentmethod:'支付宝',
+	         chaxundanPaymentdate:'2015-10-20',
+	         chaxundanMiddleman:'wwx',
+	         chaxundanPaymentamount:'wwx',
+	         chaxundanPaymentaccount:'wwx',
+	         chaxundanBankaccount:'wwx',
+	         chaxundanState:'wwx',
+	         chaxundanWriteoffamount:'wwx',
+	         chaxundanRemarks:'wwx'
 	       };
-	         $scope.data = chaxundanData;
-                 $scope.isHidden = true;
-                 $scope.showInput = function () {
-                      $scope.isHidden = !$scope.isHidden;
+	          $scope.data = chaxundanData;
+                  $scope.isHidden = true;
+                  $scope.showInput = function () {
+                  $scope.isHidden = !$scope.isHidden;
         }
-}
-);
-	
-	// use the chaxundanModule variable to
-	// configure the module with a filter
-	chaxundanModule.filter('stripDashes', function() {
-	    return function(txt) {
-	        // filter code would go here
-	}; });
+        $scope.chaxundan = {};
+        $scope.register = function () {
+            var promise = chaxundanService.doRegistration($scope.chaxundan);
+            promise.success(function (data, status, headers, config, statusText) {
+                $scope.backMess = data.success;
+                $scope.isHidden = !$scope.isHidden;
+                if(!$scope.isHidden){
+                    alert($scope.backMess[0].message + "\n" + "\n" + "Your chaxundanUnitname is " + $scope.chaxundan.Unitname);
+                }
+            });
+            promise.error(function (data, status, headers, config, statusText) {
+                $scope.backMess = data.error;
+                $scope.isHidden = !$scope.isHidden;
+                if(!$scope.isHidden){
+                    alert($scope.backMess[0].message);
+                }
+            });
+        }
+});
 
+// use the myAppModule variable to
+// configure the module with a filter
+chaxundanModule.filter('stripDashes', function() {
+    return function(txt) {
+        // filter code would go here
+}; });
