326b6f68f0cc24577ba7ac241fb2edfed289c1d9
zhongdingjun <1178090249@qq.com>
2015-12-23 17:51:55 +0800

    1314080901146 钟定钧 实验7
    实验7

--- works/1314080901146/js/factoryModule.js
+++ works/1314080901146/js/factoryModule.js
@@ -1,26 +1,51 @@
-
+﻿
 // create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
-var factoryModule = angular.module('factoryModule', []);
+var factoryModule = angular.module('factoryModule', ['ngRoute']);
 
+factoryModule.config(function ($routeProvider) {
+    // configure the routes
+    $routeProvider
+        .when('/', {
+            // route for the about page
+            templateUrl: 'pages/home.html'
+         })
+        .when('/home', {
+            templateUrl: 'pages/home.html'
+        })
+       .when('/input', {
+           templateUrl: 'pages/input.html',
+           controller: 'inputCtrl'
+       })
+       .when('/about', {
+          templateUrl: 'pages/about.html',
+           controller: 'aboutCtrl'
+      })
+      .when('/content', {
+          templateUrl: 'pages/content.html',
+          controller: 'contentCtrl'
+      });
+});
+ 
 // use the myAppModule variable to
 // configure the module with a controller
 factoryModule.factory('factoryService', function ($http) {
 	var dataStroe = {};
-        dataStroe.doRegistration = function (theData) {
+    dataStroe.doRegistration = function (theData) {
             var promise = $http({method: 'POST',url: 'json/message.json',data: theData});
             return promise;
         }
         return dataStroe;
-    }).controller('MyFilter', function ($scope,factoryService) {
+    });
+	
+factoryModule.controller('inputCtrl', function ($scope,factoryService) {
        // controller code would go here
-	var factoryData = {
-            factoryName: 'kaixin',
-            factoryNum:  '001',
-			factoryLocation: 'GuangDong',
-			factoryDescription: '信誉良好',
+	 $scope.factoryArray = {
+              factoryName: 'kaixin',
+              factoryNum:  '001',
+			  factoryLocation: 'GuangDong',
+			  factoryDescription: '信誉良好',
         };
-        $scope.data = factoryData;
 		$scope.isHidden = true;
         $scope.showInput = function () 
 		{
@@ -33,23 +58,57 @@ factoryModule.factory('factoryService', function ($http) {
                 $scope.backMess = data.success;
                 $scope.isHidden = !$scope.isHidden;
                 if(!$scope.isHidden){
-                    alert($scope.backMess[0].message + "\n" + "\n" + "Your factoryNum is " + $scope.factory.Num);
+                    alert($scope.backMess[0].message + "\n" + "\n" + "Your factoryNum is " + $scope.factoryNum);
                 }
             });
             promise.error(function (data, status, headers, config, statusText) {
-                $scope.backMess = data.error;
-                $scope.isHidden = !$scope.isHidden;
-                if(!$scope.isHidden){
-                    alert($scope.backMess[0].message);
-                }
+                $scope.backMess = 'sorry ！提交失败';
+                alert($scope.backMess);              
             });
         }
-});
-
-
+}).controller('aboutCtrl', function ($scope) {
+    var aboutData = {
+        num: '1314080901146'
+    };
+    $scope.aboutData = aboutData;
+}).controller('contentCtrl', function ($scope) {
+    $scope.factoryArray = [{
+        factoryName: 'kaixin',
+         factoryNum:  '001',
+		 factoryLocation: 'GuangDong',
+		 factoryDescription: '信誉良好'
+    }];
+ });
 // use the myAppModule variable to
 // configure the module with a filter
 factoryModule.filter('stripDashes', function() {
     return function(txt) {
         // filter code would go here
-}; });
+}; 
+});
+factoryModule.directive("myFactory", function () {
+    return {
+        restrict: "AE",
+        replace: true,
+        templateUrl: 'directives/factory.html'
+
+    }
+}).directive("myHide", function () {
+    return {
+        restrict: "AE",
+        replace: true,
+        templateUrl: 'directives/hide.html'
+    }
+}).directive("myNav", function () {
+    return {
+        restrict: "AE",
+        replace: true,
+        templateUrl: 'directives/nav.html'
+    }
+}).directive("myShow", function () {
+    return {
+        restrict: "AE",
+        replace: true,
+        template: '<div id="main"><div ng-view></div></div>'
+    }
+}); 
