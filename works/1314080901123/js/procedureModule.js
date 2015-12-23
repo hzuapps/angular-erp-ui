9142a91767540469792e0a52feb93b9ae2643753
HeyLK <374843192@qq.com>
2015-12-23 16:25:37 +0800

    实验7
    实验7

--- works/1314080901123/js/procedureModule.js
+++ works/1314080901123/js/procedureModule.js
@@ -1,24 +1,45 @@
-// create a new module called 'myAppModule' and save 
-// a reference to it in a variable called myAppModule 
+var procedureModule = angular.module('procedureModule', ['ngRoute']);
 
-alert("test")
-var procedureModule = angular.module('procedureModule', []);
-// use the myAppModule variable to
-// configure the module with a controller
-procedureModule.controller('MyFilterDemoCtrl', function ($scope) {
-       // controller code would go here
-        var procedureData = {         
-            procedureName: 'test',
-            procedureOperation: 'execute',
-            procedureCondition: 'Loading'           
-        };
-        $scope.data = procedureData;
-}
-);
+procedureModule.config(function ($routeProvider) {
+    // configure the routes
+    $routeProvider
+        .when('/', {
+            // route for the about page
+            templateUrl: 'pages/Project.html'
+        })
+        .when('/Project', {
+            templateUrl: 'pages/Project.html'
+        })
+        .when('/Home', {
+            templateUrl: 'pages/Home.html',
+            controller: 'procedureModuleDemoCtrl'
+        })
+        .when('/About', {
+            templateUrl: 'pages/About.html'
+        });       
+});
 
-// use the myAppModule variable to
-// configure the module with a filter
-procedureModule.filter('stripDashes', function() {
+procedureModule.factory('procedureModuleService',function($http){
+     var procedureData={};
+     procedureData.getData=function(){
+            var promise=$http({
+              url:"json/Database.json",
+              method:"GET"
+            });
+            return promise;
+      }
+      return procedureData;
+}).controller('procedureModuleDemoCtrl',function($scope,procedureModuleService){
+  var promise=procedureModuleService.getData();
+          promise.success(function(data,status, headers, config, statusText){
+          $scope.theOrderDatas = data.ProcessData;
+           });
+     promise.error(function(data,status){
+           alert(status);
+           alert("请求出现错误！");
+     });
+});
+PaymentDetailsModule.filter('stripDashes', function() {
     return function(txt) {
         // filter code would go here
 }; });
