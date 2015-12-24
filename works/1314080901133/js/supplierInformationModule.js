868f40a8106943054b79f4339d381c42b803287e
Wanghengdan <452950236@qq.com>
2015-12-25 00:27:04 +0800

    实验七

--- works/1314080901133/js/supplierInformationModule.js
+++ works/1314080901133/js/supplierInformationModule.js
@@ -1,15 +1,36 @@
 // create a new module called 'supplierInformationModule' and save 
 // a reference to it in a variable called supplierInformationModule 
-var supplierInformationModule = angular.module('supplierInformationModule', []);
+var supplierInformationModule = angular.module('supplierInformationModule', ['ngRoute']);
 
 // use the supplierInformationModule variable to
 // configure the module with a controller
+supplierInformationModule.config(function ($routeProvider) {
+       // configure the routes
+     $routeProvider
+     .when('/index', {
+       // route for the home page
+         templateUrl: 'title.html',
+     })
+     .when('/home', {
+       // route for the home page
+         templateUrl: 'home.html',
+         controller: 'homeController'
+     })
+     .when('/about', {
+       // route for the about page
+         templateUrl:'about.html',
+         controller:'aboutController'
+     });
+ });
 
-supplierInformationModule.controller('MyFilterDemoCtrl', function ($http,$scope) {
+supplierInformationModule.controller('aboutController', function ($scope) {
        // controller code would go here
-       $http.get("supplierInformationList_JSON.txt").success(function(someData){
-        $scope.data=someData;
-       });
+       $scope.title = ' 学号1314080901133';
+     })
+
+supplierInformationModule.controller('homeController', function ($http,$scope){
+       // controller code would go here
+        $http.get("supplierInformationList_JSON.txt").success(function(someData){$scope.data=someData;});
      })
 
 // use the supplierInformationModule variable to
@@ -19,3 +40,4 @@ supplierInformationModule.filter('stripDashes', function() {
         // filter code would go here
     };
 });
+	
