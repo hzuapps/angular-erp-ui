faf52af91acd2d4418b2c1f40612ee95d5228265
15767978771hxc <2929494460@QQ.COM>
2015-12-24 19:23:30 +0800

    实验5

--- works/1314080901113/js/chaxundingdan.js
+++ works/1314080901113/js/chaxundingdan.js
@@ -1,24 +1,29 @@
 // create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
-var myAppModule = angular.module('chaxundingdan', []);
+var chaxundingdanModule = angular.module('chaxundingdan', []);
 
 // use the myAppModule variable to
 // configure the module with a controller
-myAppModule.controller('MyFilterDemoCtrl', function ($scope) {
+chaxundingdanModule.controller('MyFilterDemoCtrl', function ($scope) {
        // controller code would go here
  
          var options = {
             order form state:'new','finish','unfinished','all',
             start time:'2015-11-1',
  		    terminal time:'2015-11-30',
-        };
+          };
          $scope.data = options;
+		 	$scope.isHidden = true;
+         $scope.showInput = function () 
+ 		{
+            $scope.isHidden = !$scope.isHidden;
+        }
     }
 );
 
 // use the myAppModule variable to
 // configure the module with a filter
-myAppModule.filter('stripDashes', function() {
+chaxundingdanModule.filter('stripDashes', function() {
     return function(txt) {
         // filter code would go here
 }; });
