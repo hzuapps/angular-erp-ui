f98345436c064735309a5afe482ab31a41875f5e
sjy503201878 <503201878@qq.com>
2015-12-25 01:16:32 +0800

    实验五

--- works/1314080901130/js/NewTypeSetModule.js
+++ works/1314080901130/js/NewTypeSetModule.js
@@ -1,22 +1,27 @@
 // create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
-var myAppModule = angular.module('NewTypeSetModule', []);
+var NewTypeSetModule = angular.module('NewTypeSetModule', []);
 
 // use the myAppModule variable to
 // configure the module with a controller
-myAppModule.controller('MyFilterDemoCtrl', function ($scope) {
+NewTypeSetModule.controller('MyFilterDemoCtrl', function ($scope) {
        // controller code would go here
        var NewTypeSet = {
-            typeName: '五金',
-            typeCode: 'wj',
+            Name: '五金',
+            Code: 'wj',
         };
         $scope.data = NewTypeSet;
+    	$scope.items = [
+            
+             {typeName: '钢材',typecode: 'gc'},
+             {typeName: '玻璃',typecode: 'bl'},
+             ];
     }
 );
 
 // use the myAppModule variable to
 // configure the module with a filter
-myAppModule.filter('stripDashes', function() {
+NewTypeSetModule.filter('stripDashes', function() {
     return function(txt) {
         // filter code would go here
 }; });
