982bc935669ce034b092cd4563ca8075054e369e
wstever <415251475@qq.com>
2015-12-26 22:50:20 +0800

    实验七

--- works/1314080901233/js/queryProcessing.js
+++ works/1314080901233/js/queryProcessing.js
@@ -1,9 +1,28 @@
 // create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
-var queryProcessing = angular.module('queryProcessing', []);
+var queryProcessing = angular.module('queryProcessing',['ngRoute']);
 
 // use the myAppModule variable to
 // configure the module with a controller
+queryProcessing.config(function ($routeProvider) {
+      // configure the routes
+    $routeProvider
+    .when('/home', {
+      // route for the home page
+        templateUrl: 'home.html',
+        controller: 'queryProcessingCtrl'
+    })
+    .when('/about', {
+      // route for the about page
+        templateUrl:'about.html',
+        controller:'aboutController'
+    });
+});
+
+queryProcessing.controller('aboutController', function ($scope) {
+       alert("温仕添-学号：1314080901233")
+    });
+
 queryProcessing.controller('queryProcessingCtrl', function ($http,$scope) {
        // controller code would go here
 
