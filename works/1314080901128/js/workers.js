506b50a91535d90b3b86812450a99d0d598afe54
hzukondy <493030390@qq.com>
2015-12-26 23:02:06 +0800

    实验7

--- works/1314080901128/js/workers.js
+++ works/1314080901128/js/workers.js
@@ -1,16 +1,41 @@
 // create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
-var myAppModule = angular.module('workersModule', []);
+var myAppModule = angular.module('workersModule', ['ngRoute']);
 
 // use the myAppModule variable to
 // configure the module with a controller
-myAppModule.controller('MyFilterDemoCtrl', function ($http,$scope) {
-        $http.get("shuju_JSON.txt").success(function(someData){
-        $scope.data = someData;
-       // controller code would go here
-        });
-})
 
+ myAppModule.config(function ($routeProvider) {
+      // configure the routes
+    $routeProvider
+    .when('/name', {
+      // route for the home page
+        templateUrl: 'title.html'
+    })
+    .when('/home', {
+      // route for the home page
+        templateUrl: 'home.html'
+    })
+    .when('/contact', {
+      // route for the home page
+        templateUrl: 'contact.html',
+        controller: 'contactController'
+    })
+    .when('/about', {
+      // route for the about page
+        templateUrl:'about.html'
+    });
+});
+
+
+
+
+
+myAppModule.controller('contactController', function ($http,$scope) {
+       // controller code would go here
+       $http.get("shuju_JSON.json").success(function(someData){$scope.data=someData;});
+     
+    });
 
 
 // use the myAppModule variable to
