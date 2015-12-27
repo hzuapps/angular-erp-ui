8727216ea7eafab46dfb3960032c42dc21aa999f
sjy503201878 <503201878@qq.com>
2015-12-27 20:42:46 +0800

    实验6

--- works/1314080901130/js/NewTypeSetModule.js
+++ works/1314080901130/js/NewTypeSetModule.js
@@ -4,18 +4,33 @@ var NewTypeSetModule = angular.module('NewTypeSetModule', []);
 
 // use the myAppModule variable to
 // configure the module with a controller
-NewTypeSetModule.controller('MyFilterDemoCtrl', function ($scope) {
+NewTypeSetModule.factory('myService',function($http){
+         var dataStroe = {};
+         dataStroe.getData = function () {
+             var promise = $http({method: 'GET',url: 'json/data.json'});
+             return promise;
+        }
+         return dataStroe;
+     }).controller('MyFilterDemoCtrl', function ($scope,myService) {
        // controller code would go here
        var NewTypeSet = {
             Name: '五金',
             Code: 'wj',
         };
         $scope.data = NewTypeSet;
-    	$scope.items = [
+    	/*$scope.items = [
             
              {typeName: '钢材',typecode: 'gc'},
              {typeName: '玻璃',typecode: 'bl'},
              ];
+        */
+        var promise = myService.getData();
+        promise.success(function (data, status, headers, config, statusText) {
+            $scope.items = data.items;
+        });
+        promise.error(function(data,status){
+            alert('请求出错，请重试');
+        });
     }
 );
 
