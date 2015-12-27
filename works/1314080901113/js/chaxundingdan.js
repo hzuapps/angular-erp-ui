dbc8abcf94b41905f9548d04aec69d89e756c230
15767978771hxc <2929494460@QQ.COM>
2015-12-27 20:54:27 +0800

    实验6

--- works/1314080901113/js/chaxundingdan.js
+++ works/1314080901113/js/chaxundingdan.js
@@ -4,26 +4,46 @@ var chaxundingdanModule = angular.module('chaxundingdan', []);
 
 // use the myAppModule variable to
 // configure the module with a controller
-chaxundingdanModule.controller('MyFilterDemoCtrl', function ($scope) {
-       // controller code would go here
+chaxundingdanModule.controller('chaxundingdanService', function ($http) {
+      var dataStroe = {};
+         dataStroe.doRegistration = function (theData) {
+             var promise = $http({method: 'POST',url: 'json/message.json',data: theData});
+             return promise;
+         }
+         return dataStroe;
+     }).controller('MyFilter', function ($scope,chaxundingdanService) {
+        // controller code would go here
+         var chaxundingdanData = {
+             
+         };
+         $scope.data = chaxundingdanData;
+         $scope.isHidden = true;
+         $scope.showInput = function () {
+             $scope.isHidden = !$scope.isHidden;
+         }
+         $scope.chaxundingdan = {};
+         $scope.register = function () {
+             var promise = chaxundingdanService.doRegistration($scope.chaxundingdan);
+             promise.success(function (data, status, headers, config, statusText) {
+                 $scope.backMess = data.success;
+                 $scope.isHidden = !$scope.isHidden;
+                 if(!$scope.isHidden){
+                     alert($scope.backMess[0].message + "\n" + "\n" );
+                 }
+             });
+             promise.error(function (data, status, headers, config, statusText) {
+                 $scope.backMess = data.error;
+                 $scope.isHidden = !$scope.isHidden;
+                 if(!$scope.isHidden){
+                     alert($scope.backMess[0].message);
+                 }
+             });
+         }
+ });
  
-         var options = {
-            order form state:'new','finish','unfinished','all',
-            start time:'2015-11-1',
- 		    terminal time:'2015-11-30',
-          };
-         $scope.data = options;
-		 	$scope.isHidden = true;
-         $scope.showInput = function () 
- 		{
-            $scope.isHidden = !$scope.isHidden;
-        }
-    }
-);
-
-// use the myAppModule variable to
-// configure the module with a filter
-chaxundingdanModule.filter('stripDashes', function() {
-    return function(txt) {
-        // filter code would go here
-}; });
+ // use the myAppModule variable to
+ // configure the module with a filter
+ chaxundingdanModule.filter('stripDashes', function() {
+     return function(txt) {
+         // filter code would go here
+ }; }); 
