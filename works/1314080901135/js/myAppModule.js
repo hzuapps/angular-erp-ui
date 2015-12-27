0d56493438b32c77a03f642f2d0eb60d9c6f5dbb
wxuanguang <247017271@qq.com>
2015-12-27 21:36:35 +0800

    实验7

--- works/1314080901135/js/myAppModule.js
+++ works/1314080901135/js/myAppModule.js
@@ -1,6 +1,24 @@
 // create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
-var myAppModule = angular.module('myAppModule', []);
+var myAppModule = angular.module('myAppModule', ['ngRoute']);
+
+myAppModule.config(function ($routeProvider){
+  $routeProvider
+       .when('/',{
+         templateUrl:'pages/home.html'
+       })
+       .when('/home',{
+         templateUrl:'pages/home.html'
+       })
+       .when('/contact',{
+         templateUrl:'pages/contact.html',
+         controller:'myAppModuleCtrl'
+       })
+       .when('/about',{
+         templateUrl:'pages/about.html',
+         controller:'aboutCtrl'
+       });
+});
 
 // use the myAppModule variable to
 // configure the module with a controller
@@ -16,6 +34,10 @@ myAppModule.factory("myAppModuleService",function($http){
     promise.success(function(data,status){
        $scope.datas = data.typeSet; 
     });
+}).controller('aboutCtrl', function ($scope,myAppModuleService){
+    $scope.data = {
+      num: '1314080901135'
+    };
 });
 
 // use the myAppModule variable to
