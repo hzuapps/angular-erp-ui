d4ed5844b4e3be55a6545d8a4f711f13317c5271
lst1297907919 <1297907919@qq.com>
2015-12-27 22:52:52 +0800

    实验7

--- works/1314080901122/js/newShoppinglistModule.js
+++ works/1314080901122/js/newShoppinglistModule.js
@@ -1,6 +1,26 @@
 // create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
-var newShoppinglistModule = angular.module('newShoppinglistModule', []);
+var newShoppinglistModule = angular.module('newShoppinglistModule', ['ngRoute']);
+
+newShoppinglistModule.config(function ($routeProvider) {
+    // configure the routes
+    $routeProvider
+        .when('/', {
+            // route for the about page
+            templateUrl: 'pages/home.html'
+        })
+        .when('/home', {
+            templateUrl: 'pages/home.html'
+        })
+        .when('/contact', {
+            templateUrl: 'pages/shop.html',
+            controller: 'MyFilterDemoCtrl'
+        })
+        .when('/about', {
+            templateUrl: 'pages/about.html',
+            controller: 'aboutCtrl'
+        });
+});
 
 // use the myAppModule variable to
 // configure the module with a controller
@@ -20,9 +40,12 @@ newShoppinglistModule.factory('newShoppinglistService',function($http){
          promise.error(function(data,status){
             alert('请求出错，请重试');
          });
-    }
-
-);
+}).controller('aboutCtrl', function ($scope) {
+        var data = {
+            num: '1314080901122'
+        };
+        $scope.data = data;
+});
 
 // use the myAppModule variable to
 // configure the module with a filter
