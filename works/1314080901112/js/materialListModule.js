841127f4e3302d90dee087776214c0eb6039638c
hh-froal <15919568335hh>
2015-12-28 10:30:59 +0800

    1314080901112 实验7

--- works/1314080901112/js/materialListModule.js
+++ works/1314080901112/js/materialListModule.js
@@ -1,16 +1,37 @@
 // create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
-var materialListModule = angular.module('materialListModule', []);
+var materialListModule = angular.module('materialListModule', ['ngRoute']);
 
 // use the myAppModule variable to
 // configure the module with a controller
-materialListModule.controller('MyFilterDemoCtrl', function ($http,$scope) {
-        $http.get("item.json").success(function(someData){
-        $scope.data = someData;
-       // controller code would go here
-        });
-})
+ materialListModule.config(function ($routeProvider) {
+      // configure the routes
+    $routeProvider
+    .when('/name', {
+      // route for the home page
+        templateUrl: 'title.html'
+    })
+    .when('/home', {
+      // route for the home page
+        templateUrl: 'home.html'
+    })
+    .when('/contact', {
+      // route for the home page
+        templateUrl: 'contact.html',
+        controller: 'contactController'
+    })
+    .when('/about', {
+      // route for the about page
+        templateUrl:'about.html'
+    });
+});
 
+
+materialListModule.controller('contactController', function ($http,$scope) {
+       // controller code would go here
+       $http.get("item.json").success(function(someData){$scope.data=someData;});
+     
+    });
 // use the myAppModule variable to
 // configure the module with a filter
 materialListModule.filter('stripDashes', function() {
