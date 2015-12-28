7fd48f3fadc4faf7bd23939fed7f093d1f1cd8c4
1314080901137 <417015301@qq.com>
2015-12-28 12:32:41 +0800

    实验七

--- works/1314080901137/js/cangkucaigoushouhuoModule.js
+++ works/1314080901137/js/cangkucaigoushouhuoModule.js
@@ -1,21 +1,42 @@
 // create a new module called 'cangkucaigoushouhuoModule' and save 
 // a reference to it in a variable called myAppModule 
-var cangkucaigoushouhuoModule = angular.module('cangkucaigoushouhuoModule', []);
+var cangkucaigoushouhuoModule = angular.module('cangkucaigoushouhuoModule', ['ngRoute']);
 
 // use the myAppModule variable to
-// configure the module with a controller
+// configure he module with a controller
 
-cangkucaigoushouhuoModule.factory('prividerService',function($http){
-   var dataSvc={};
-   dataSvc.getData=function(theData){
-    var promise=$http({method: 'POST',url: 'ckjson/cangkucaigoushouhuodata.json',data: theData});
+cangkucaigoushouhuoModule.config(function ($routeProvider) {
+    // configure the routes
+    $routeProvider
+        .when('/', {
+            // route for the about page
+            templateUrl: 'pages/home.html'
+        })
+        .when('/home', {
+            templateUrl: 'pages/home.html'
+        })
+        .when('/contact', {
+            templateUrl: 'pages/aa.html',
+            controller: 'cakuDetailCtrl'
+        })
+        .when('/about', {
+            templateUrl: 'pages/about.html',
+            controller: 'aboutCtrl'
+        });
+});
+
+
+cangkucaigoushouhuoModule.factory('cakuDetailService',function($http){
+   var dataStroe={};
+   dataStroe.getData=function(){
+    var promise=$http({method: 'GET',url: 'json/data.json'});
     return promise;
   }
-    return dataSvc;
+    return dataStroe;
 
-}).controller('MyFilterDemoCtrl', function ($scope,prividerService) {
+}).controller('cakuDetailCtrl', function ($scope,cakuDetailService) {
        // controller code would go here
-                var cat = {
+                var cakuDetail = {
             gongyingshang: 'china',
             cangku: '001',
             jingshouren:'zhang3',
@@ -23,36 +44,56 @@ cangkucaigoushouhuoModule.factory('prividerService',function($http){
             zhuangtai:'ok',
             beizhu:' '  
         };
-        $scope.data = cat;
-         $scope.isHidden = true;
-        $scope.showInput = function () {
-            $scope.isHidden = !$scope.isHidden;
-        }
-$scope.register=function(){
-              var promise=prividerService.getData($scope.provider);
-              promise.success(function(data,status,headers,config,statusText){
-                  $scope.backMess=data.success;
-                  $scope.isHidden = !$scope.isHidden;
-                  if (!$scope.isHidden) {
-                    alert($scope.backMess[0].message+"\n");
-                  }
-              });
-                promise.error(function(data,status,headers,config,statusText){
-                    $scope.backMess=data.error;
-                    $scope.isHidden = !$scope.isHidden;
-                    if (!$scope.isHidden) {
-                        alert($scope.backMess[0].message);
-                    }
-                });
-        }
+        $scope.data = cakuDetail;
+ $scope.items = [
+            { "序号":"0",
+            "编码":"000", 
+            "类别":"男装", 
+            "名称":"box",
+            "型号":"m",
+            "长":"1.0",
+            "宽":"1.0",
+            "厚":"1.0",
+            "单位":"件",
+            "数量":"10"   },
+            { "序号":"1",
+            "编码":"001", 
+            "类别":"男装", 
+            "名称":"box",
+            "型号":"l",
+            "长":"2.0",
+            "宽":"2.0",
+            "厚":"2.0",
+            "单位":"件",
+            "数量":"20"   },
+            {"序号":"2",
+            "编码":"002", 
+            "类别":"男装", 
+            "名称":"box",
+            "型号":"xl",
+            "长":"3.0",
+            "宽":"3.0",
+            "厚":"3.0",
+            "单位":"件",
+            "数量":"50"    } 
+        ];
+        var promise =cakuDetailService.getData();
+        promise.success(function (data, status, headers, config, statusText) {
+            $scope.backMess = data.cangkucaigoushouhuodata;
+        });
+        promise.error(function(data,status){
+           alert('请求出错，请重试');
+        });
+    }).controller('aboutCtrl', function ($scope) {
+        var data = {
+            num: '1314080901137'
+        };
+        $scope.data = data;
 });
-    
-
 
 // use the myAppModule variable to
 // configure the module with a filter
 cangkucaigoushouhuoModule.filter('stripDashes', function() {
     return function(txt) {
         // filter code would go here
-}; 
-});//javascript.js
+}; });
