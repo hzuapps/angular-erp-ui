8f5c8b0b99599abf0cfcd43986da4bd1039c5335
mumuLOVEanni <245189703@qq.com>
2015-12-28 14:53:45 +0800

    实验六

--- works/1314080901147/js/FinancialsalesModule.js
+++ works/1314080901147/js/FinancialsalesModule.js
@@ -1,25 +1,55 @@
-// create a new module called 'FinancialsalesModule' and save 
-// a reference to it in a variable called FinancialsalesModule
+
+// create a new module called 'myAppModule' and save 
+// a reference to it in a variable called myAppModule 
 var FinancialsalesModule = angular.module('FinancialsalesModule', []);
 
-// use the FinancialsalesModule variable to
+// use the myAppModule variable to
 // configure the module with a controller
-FinancialsalesModule.controller('MyfilterDemoCtrl', function ($scope) {
+FinancialsalesModule.factory('saleService', function ($http) {
+	var datasale = {};
+        datasale.getdata = function (theData) {
+            var promise = $http({method: 'POST',url: 'json/message.json',data: theData});
+            return promise;
+        }
+        return datasale;
+    }).controller('MyFilter', function ($scope,saleService) {
        // controller code would go here
-      var Financialsales1 = {
-            orderNumber:'123',
+	var FinancialsalesData = {
+             orderNumber:'123',
             ordername:'123',
             summary:'abc',
             moneyed:'123',
             money:'123',
             beizhu:'abc',
-            
         };
-        $scope.data = Financialsales1;
-    }
-);
+        $scope.data = FinancialsalesData;
+		$scope.isHidden = true;
+        $scope.showInput = function () 
+		{
+            $scope.isHidden = !$scope.isHidden;
+        }
+		$scope.Financialsales = {};
+        $scope.register = function () {
+            var promise = saleService.getdata ($scope.Financialsales);
+            promise.success(function (data, status, headers, config, statusText) {
+                $scope.backMess = data.success;
+                $scope.isHidden = !$scope.isHidden;
+                if(!$scope.isHidden){
+                    alert($scope.backMess[0].message + "\n" );
+                }
+            });
+            promise.error(function (data, status, headers, config, statusText) {
+                $scope.backMess = data.error;
+                $scope.isHidden = !$scope.isHidden;
+                if(!$scope.isHidden){
+                    alert($scope.backMess[0].message);
+                }
+            });
+        }
+});
 
-// use the Financialsales variable to
+
+// use the myAppModule variable to
 // configure the module with a filter
 FinancialsalesModule.filter('stripDashes', function() {
     return function(txt) {
