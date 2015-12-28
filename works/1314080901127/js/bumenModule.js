e961b44d9266da1b40f7acd0d452f02825d33d37
phm <632305424@qq.com>
2015-12-28 14:45:42 +0800

    实验6+实验7

--- works/1314080901127/js/bumenModule.js
+++ works/1314080901127/js/bumenModule.js
@@ -1,24 +1,34 @@
-// create a new module called 'myAppModule' and save 
-// a reference to it in a variable called myAppModule 
 var bumenModule = angular.module('bumenModule', []);
 
 // use the myAppModule variable to
 // configure the module with a controller
-bumenModule.controller('MyFilterDemoCtrl', function ($scope) {
-       // controller code would go here
-       var bumen = {
+bumenModule.controller('MyFilterDemoCtrl', function ($scope,$http) {
+       var promise=$http({
+               url:"json/phm.json",
+                method:"get",
+              });
+        promise.success(function(data,status){
+        alert(status);
+ 
+          var bu = {
             name: '123',
             code: '123',
-            workernumber: 3
+            workernumber: '3',
         };
-        $scope.data = bumenData;
-		$scope.isHidden = true;
-        $scope.showInput = function () 
-		{
-            $scope.isHidden = !$scope.isHidden;
-        }
-}
-);
+          $scope.data =bu;
+           $scope.isHidden = true;
+         $scope.show = function(){
+             $scope.isHidden = !$scope.isHidden;
+             }
+       });
+       promise.error(function(data,status){
+         alert(status);
+       });
+        // controller code would go here
+     
+      }
+  );
+
 
 // use the myAppModule variable to
 // configure the module with a filter
