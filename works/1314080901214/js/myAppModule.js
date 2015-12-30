d46f5fee97181897ff6bcac4e7a6c67d10e474bd
11hks11 <912702351@qq.com>
2015-12-30 20:29:39 +0800

    实验4

--- works/1314080901214/js/myAppModule.js
+++ works/1314080901214/js/myAppModule.js
@@ -5,14 +5,27 @@ var myAppModule = angular.module('myAppModule', []);
 
 // use the myAppModule variable to
 // configure the module with a controller
+var documentsDetail = {
+                 laiyuan:"laiyuan1",
+                 leibie:"leibie1",
+                 mingchen:"mingchen1",
+                 xinhao:"xinhao1",
+                 yanse:"red",
+                 changdu:"changdu1",
+                 kuandu:"kuandu1",
+                 houdu:"houdu1",
+                 zhuangtai:"zhuangtai1",
+                 kucunshu:"kucunshu1",
+                 xuqiushu:"xuqiushu1",
+                 caigoushu:"caigoushu1",
+                 BOM:"bom1"
+                };
+
 myAppModule.controller('MyFilterDemoCtrl', function ($scope) {
        // controller code would go here
-         var documentsDetail = {
-                xuqiu: '001',
-                xuqiuchaxun: 'shantou',
-                remark: 'none',
-                };
-        $scope.data = documentsDetail;
+         
+        $scope.data1 = documentsDetail;
+        
     }
 );
 
