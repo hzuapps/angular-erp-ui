fc50654e02707bf7f1d30c071eb24ed10d406614
Robin30517 <zhcbin520@qq.com>
2016-01-03 10:37:52 +0800

    1314080901241张潮彬 实验七
    1314080901241张潮彬 实验七

--- works/1314080901241/js/robinModule.js
+++ works/1314080901241/js/robinModule.js
@@ -1,16 +1,37 @@
  // create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
-var robinModule = angular.module('robinModule', []);
+
+var robinModule = angular.module('robinModule', ['ngRoute']);
 
 // use the myAppModule variable to
 // configure the module with a controller
-
-robinModule.controller('createDemoCtrl', function ($http,$scope) {
-       // controller code would go here
-       $http.get("bjList_JSON.txt").success(function(someData){
-        $scope.data=someData;
-       });
+robinMoudle.config(function ($routeProvider) {
+       // configure the routes
+     $routeProvider
+     .when('/index', {
+     // route for the home page
+         templateUrl: 'title.html',
      })
+     .when('/home', {
+       // route for the home page
+         templateUrl: 'home.html',
+         controller: 'homeController'
+     })
+     .when('/about', {
+       // route for the about page
+         templateUrl:'about.html',
+        controller:'aboutController'
+     });
+});
+
+
+robinModule.controller('aboutController', function ($scope) {
+         $scope.title = ' Ñ§ºÅ1314080901241';
+    });
+robinModule.controller('homeController', function ($http,$scope) {
+$http.get("materialList_JSON.txt").success(function(someData){$scope.data=someData;});
+      
+    });
 
 
 // use the myAppModule variable to
