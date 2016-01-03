d2bfe2c3639ad4cac628420a97e8ef37d6a72544
pilimachao <745728619@qq.com>
2016-01-04 00:40:12 +0800

    实验5

--- works/1314080901238/js/orderModule.js
+++ works/1314080901238/js/orderModule.js
@@ -18,6 +18,29 @@ myOrder.controller('myController', function ($scope) {
         $scope.myData=myData;
     });
 
+
+myOrder.directive("myorderdata",function(){
+  return{
+      restrict:"AEC",
+      template:'<table>'
+              +'<tr><td>名称：</td></tr>'
+              +'<tr><td colspan="2" ><input class="form-control" type="text" id="name" ng-model="myData.name"></td><td></td></tr>'
+              +'<tr><td>代号：</td></tr>'
+              +'<tr><td colspan="2" ><input class="form-control" type="text" id="number" ng-model="myData.number"></td><td></td></tr>'
+              +'<tr><td>位置：</td></tr>'
+              +'<tr><td colspan="2" ><input class="form-control" type="text" id="position" ng-model="myData.position"></td><td></td></tr>'
+              +'<tr><td >描述：</td></tr>'
+              +'<tr><td colspan="1" ><textarea class="form-control" name="details" id="details" rows="4" cols="100%" ng-model="myData.details">'
+                  +'</textarea><td></td></tr>'
+              +'</table>',
+      transclude: true
+  };
+});
+
+
+
+
+
 // use the myAppModule variable to
 // configure the module with a filter
 myOrder.filter('stripDashes', function () {
