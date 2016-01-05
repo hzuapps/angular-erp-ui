842f356df835291f3ec91c2d9c88f9d074a83943
pengjianlin <1053341361@qq.com>
2016-01-05 22:28:07 +0800

    第七次实验

--- works/1314080901226/js/feiyongdanModule.js
+++ works/1314080901226/js/feiyongdanModule.js
@@ -1,10 +1,31 @@
 
 // create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
-var feiyongdanModule = angular.module("feiyongdanModule", []);
+var feiyongdanModule = angular.module("feiyongdanModule", ['ngRoute']);
 // use the myAppModule variable to
 // configure the module with a controller
-feiyongdanModule.controller('feiyongdanModuleCtrl', function ($http,$scope) {
+feiyongdanModule.config(function ($routeProvider) {
+      // configure the routes
+    $routeProvider
+    .when('/index', {
+      // route for the home page
+        templateUrl: 'title.html',
+    })
+    .when('/home', {
+      // route for the home page
+        templateUrl: 'home.html',
+        controller: 'homeController'
+    })
+    .when('/about', {
+      // route for the about page
+        templateUrl:'about.html',
+        controller:'aboutController'
+    });
+});
+feiyongdanModule.controller('aboutController', function ($scope) {
+      $scope.title = ' ID 1314080901226';
+    });
+feiyongdanModule.controller('homeController', function ($http,$scope) {
        // controller code would go here
        $http.get("feiyongdan_JSON.txt").success(function(someData){
         $scope.data=someData;
