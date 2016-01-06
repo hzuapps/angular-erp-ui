7f3de91d17aa15d5cd5a4da3dc85639fab2c0c1c
jayhoo95 <1063200319@qq.com>
2016-01-06 21:57:37 +0800

    实验7

--- works/1314080901236/js/myAppModule.js
+++ works/1314080901236/js/myAppModule.js
@@ -1,29 +1,126 @@
-// create a new module called 'myAppModule' and save 
-// a reference to it in a variable called myAppModule 
-var myAppModule = angular.module('myAppModule', []);
+﻿//初始化表单数据
 
-// use the myAppModule variable to
-// configure the module with a controller
-myAppModule.controller('SuppInfor', function ($scope)
-	{
-		var SuppInfor= 
+var SuppInfor= 
 			{  SuppId: '001',
-			    CreatDate: new Date(2015, 10, 18),
+			    CreatDate:'2015-10-23' ,
 			    SuppType: '五金类',
 			    SuppFullName: '供应商全称',
 			    pinyincode: 'null',
-			    SuppPage: 'http://',
+			    SuppPage: ' ',
 			    SuppShortName:'供应商简称',
 			    AdministrativeDivision :'行政区域',
 			    SuppDesc :'供应商简介',
 			};
-			$scope.data = SuppInfor;
-	}
-);
 
-// use the myAppModule variable to
-// configure the module with a filter
-myAppModule.filter('stripDashes', function() {
-    return function(txt) {
-        // filter code would go here
-}; });
+
+//创建模块
+var myAppModule = angular.module("myAppModule", ['ngRoute']);
+      
+      myAppModule.config(['$routeProvider',
+         function($routeProvider) {
+            $routeProvider.
+               when('/myWebsite', {
+                  templateUrl: 'pages/myWebsite.html',
+                  //controller: 'myWebsiteController'
+               }).
+			   when('/tableData', {
+                  templateUrl: 'pages/tableData.html',
+                  //controller: 'myWebsiteController'
+               }).
+               when('/aboutMe', {
+                  templateUrl: 'pages/aboutMe.html',
+                  //controller: 'aboutMeController'
+               }).
+               otherwise({
+                  redirectTo: '/myWebsite'
+               });
+         }]);
+
+         /*myAppModule.controller('myWebsiteController', function($scope) {
+            $scope.message = "Welcome to my Website";
+         });
+         myAppModule.controller('aboutMeController', function($scope) {
+            $scope.message = "machaorong 1314080901225";
+         });*/
+
+
+
+//创建服务
+myAppModule.factory("AppService",function($http){
+      var getmyAppData={};
+      
+	  //getData方法
+      getmyAppData.getData=function(buttondata){
+            var promise=$http({
+              url:"json/buttonData.json",
+              method:"GET",
+			  data:buttondata
+            });
+            return promise;
+      }
+      
+      return getmyAppData;
+
+});
+
+//定义控制器
+
+angular.module('myAppModule').controller('MyFilterDemoCtrl', function($scope,AppService) {
+          var promise=AppService.getData();
+		  
+		  $scope.create = function () {
+			$scope.showSuccessMessage=false;
+			$scope.showErrorMessage=false;
+			//promise的成功方法
+			promise.success(function(data,status){
+			
+		  
+			$scope.successMessage =data[0].SMessage;
+			alert($scope.successMessage);
+   		    //$scope.showSuccessMessage=true;
+			});
+		  
+			//promise的失败方法
+			promise.error(function(data,status){
+		  
+		  
+			$scope.errorsMessage =data[0].EMessage;
+			alert($scope.errorsMessage);
+		  
+			});
+		  }
+
+			
+			
+			$scope.SelectItem = [
+			 {
+			    SuppType: '五金类',
+			 },
+			 {
+			    SuppType: '电器类',
+			 },
+			 {
+			    SuppType: '板材类',
+			 },
+			 {
+			    SuppType: '油漆类',
+			 },
+			 {
+			    SuppType: '刀具类',
+			 },
+			 {
+			    SuppType: '其他',
+			 },
+			];
+			
+
+});
+
+
+
+//定义过滤器
+ManifestDataModule.filter('stripDashes', function() {
+	return function(txt) {
+		// filter code would go here
+	};
+});
