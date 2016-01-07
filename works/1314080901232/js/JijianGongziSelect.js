73ffc1eb8a38029addcb6b3f8b50ac6ad40233d7
YUPING02 <391392705@qq.com>
2016-01-07 16:07:27 +0800

    修改实验六    增加实验七 1314080901232

---
+++ works/1314080901232/js/JijianGongziSelect.js
@@ -0,0 +1,43 @@
+﻿ // create a new module called 'myAppModule' and save 
+// a reference to it in a variable called myAppModule 
+var wuliaoSelect = angular.module('JijianGongziSelect', ['ngRoute']);
+
+// use the myAppModule variable to
+// configure the module with a controller
+ JijianGongziSelect.config(function ($routeProvider) {
+      // configure the routes
+    $routeProvider
+    .when('/index', {
+      // route for the home page
+        templateUrl: 'title.html',
+    })
+    .when('/home', {
+      // route for the home page
+        templateUrl: 'home.html',
+        controller: 'homeController'
+    })
+    .when('/about', {
+      // route for the about page
+        templateUrl:'about.html',
+        controller:'aboutController'
+    });
+});
+
+wuliaoSelect.controller('aboutController', function ($scope) {
+      $scope.title = ' 学号1314080901232';
+    });
+
+
+JijianGongziSelect.controller('homeController', function ($http,$scope) {
+       // controller code would go here
+       $http.get("materialList_JSON.txt").success(function(someData){$scope.data=someData;});
+     
+    });
+
+
+// use the myAppModule variable to
+// configure the module with a filter
+JijianGongziSelect.filter('stripDashes', function() {
+    return function(txt) {
+        // filter code would go here
+}; });
