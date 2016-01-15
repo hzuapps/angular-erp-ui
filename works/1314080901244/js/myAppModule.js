aee4d81a211b6ae609d4b56b1aa032d6cefe3731
YashonCheung <1446734764@qq.com>
2016-01-15 12:03:33 +0800

    实验七
    完成实验

--- works/1314080901244/js/myAppModule.js
+++ works/1314080901244/js/myAppModule.js
@@ -1,23 +1,60 @@
 
 // create a new module called 'myAppModule' and save 
-// a reference to it in a variable called myAppModule 
-var myAppModule = angular.module('myAppModule', []);
+// a reference to it in a variable called myAppModule
+var groupData={FunctionName:11111111,OfficerName:11111111,NewGroName:11111111,WorkShop:11111111,};
 
+var myAppModule = angular.module('myAppModule', ['ngRoute']);
+    function theRoute($routeProvider){
+      $routeProvider.when("/",{
+        templateUrl:"pages/home.html",
+        controller:"homeController"
+      })
+      .when("/about",{
+        templateUrl:"pages/about.html",
+        controller:"aboutController"
+      })
+      .otherwise("/contact",{
+        templateUrl:"pages/contact.html",
+        controller:"contactController"
+      });
+    }
 // use the myAppModule variable to
 // configure the module with a controller
-myAppModule.controller('MyFilterDemoCtrl', function ($scope) {
+myAppModule.controller("homeController",function($scope){
+  $scope.message="Welcome to Yashon's home page!"
+});
+myAppModule.controller("aboutController",function($scope){
+  $scope.name="Name:Yashon Cheung";
+  $scope.numbers="Numbers:1314080901244";
+});
+myAppModule.controller("contactController",function($scope){
+  $scope.messge="Contact us!";
+});
+myAppModule
+
+myAppModule.factory('myService',function($http){
+
+  var dataStroe={};
+
+  dataStroe.newdata=function(theData){
+    var promise=$http({
+      method:'POST',
+      url:'json/message.json',
+      data:theData
+    });
+
+    return promise;
+
+  }
+  return dataStroe;
+
+}).controller('MyFilterDemoCtrl', function ($scope,myService) {
        // controller code would go here
       var someData={
-        FunctionName:'新增工作组',
-        OfficerName:'某某某某',
-        NewGroName:'新增工作组',
-        Save:'001',
-        WorkShop:{
-            WorkShop1:'木工车间',
-            WorkShop2:'油漆车间',
-            WorkShop3:'包装车间',
-            WorkShop4:'橱柜车间',
-               }
+        FunctionName:11111111,
+        OfficerName:11111111,
+        NewGroName:11111111,
+        WorkShop:11111111,
       };
       $scope.data=someData;
     }
