4973810d39e162ec3d7f88e5ed383f8284db3b92
TPGorTJK <779446297@qq.com>
2016-01-16 01:48:25 +0800

    实验5

--- works/1314080901132/js/myModule.js
+++ works/1314080901132/js/myModule.js
@@ -13,5 +13,18 @@ var myModule = angular.module("myapp", []);
       data1.classNumber = "12";
       $scope.datas = [data1];
   });
+
+myModule.directive("mydata",function(){
+	return{
+		restrict:"AEC",
+		template:'<div style="display:inline-block;width:100px;">类别名称</div> <div style="display:inline-block;width:100px;">类别代码</div>'
+            +'<div ng-repeat="theData in datas">'
+              +'<div style="display:inline-block;width:100px;">{{theData.theClassName}}</div>'
+               +'<div style="display:inline-block;width:100px;">{{theData.classNumber}}</div>'
+             +'</div>',
+		transclude: true
+	};
+});
+
   
  
