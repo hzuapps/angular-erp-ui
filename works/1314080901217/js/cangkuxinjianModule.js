d99a8c7e06e4e052635a7952d06bd18656792749
shuo <495978900@qq.com>
2016-01-15 12:56:05 +0800

    shiyan5

--- works/1314080901217/js/cangkuxinjianModule.js
+++ works/1314080901217/js/cangkuxinjianModule.js
@@ -1,5 +1,6 @@
 ﻿// create a new module called 'myAppModule' and save 
 // a reference to it in a variable called myAppModule 
+
 var myAppModule = angular.module('cangkuxinjianModule', []);
 
 // use the myAppModule variable to
@@ -15,6 +16,43 @@ myAppModule.controller('MyFilterDemoCtrl', function ($scope) {
 
     }
 );
+//开始日期
+myAppModule.directive("starttime",function(){
+    return{
+        restrict:"AEC",
+        template:"<input id='startTime' type='text'>",
+        transclude:true
+
+    }
+});
+//结束日期
+myAppModule.directive("endtime",function(){
+    return{
+        restrict:"AEC",
+        template:"<input type='text'>",
+        transclude:true
+
+    }
+});
+//操作员
+myAppModule.directive("opera",function(){
+    return{
+        restrict:"AEC",
+        template:"<input type='text'>",
+        transclude:true
+
+    }
+});
+//备注
+myAppModule.directive("notes",function(){
+    return{
+        restrict:"AEC",
+        template:"<input type='text' style='width:500px;''>",
+        transclude:true
+
+    }
+});
+
 // use the myAppModule variable to
 // configure the module with a filter
 myAppModule.filter('stripDashes', function() {
